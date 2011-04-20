bcf8c7f8b50caa879394880e3d1a3b16a5360e5f
Stefan Armbruster <stefan@armbruster-it.de>
2011-04-20 11:13:29 +0200

    configured root mapping and output of mysql connection data upon bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,7 @@
 class BootStrap {
 
     def init = { servletContext ->
+		println "VCAP_SERVICES: ${System.getenv('VCAP_SERVICES')}"
     }
     def destroy = {
     }
--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,7 +7,8 @@ class UrlMappings {
 			}
 		}
 
-		"/"(view:"/index")
+		//"/"(view:"/index")
+		"/"(uri:"/dbconsole")
 		"500"(view:'/error')
 	}
 }
